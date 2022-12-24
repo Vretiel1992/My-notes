b54f396d6eab74c6a3a0691bb9280b65e3d385ee
Anton Denisyuk <vretiel@icloud.com>
2022-12-24 17:37:01 +0300

    initial commit

---
+++ TestTaskCFT/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  TestTaskCFT
+//
+//  Created by Антон Денисюк on 24.12.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ TestTaskCFT/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TestTaskCFT
+//
+//  Created by Антон Денисюк on 24.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemIndigo
+    }
+}
